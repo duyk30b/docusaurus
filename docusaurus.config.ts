82109a5f0940cfb35c5db76a40b69bf4183759b6
duyk30b <duyk30b@gmail.com>
2024-06-29 23:43:17 +0700

    update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -131,7 +131,7 @@ const config: Config = {
         // },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "/tools/editor.3.0.html",
+          href: "/html/editor.3.0.html",
           label: "HTMLEditor.3.0",
           position: "right",
         },
